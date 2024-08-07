4edc828ac3a7e4b7aa3a75392ae7592466f4f14e
Donsaidi <99283838+Donsaidi@users.noreply.github.com>
2024-08-07 12:55:02 +0200

    Changed mail to avoid spam

--- scripts.js
+++ scripts.js
@@ -1,6 +1,6 @@
 function generateEmailLink() {
-    var user = "jihad-s";
-    var domain = "live.it";
+    var user = "jihad.saidi";
+    var domain = "alumni.epfl.ch";
     var element = document.getElementById("email-link");
     var email = user + "@" + domain;
     element.setAttribute("href", "mailto:" + email);
